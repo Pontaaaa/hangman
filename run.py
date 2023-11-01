d086e799013dd4a065e76b051a9d93babb63349e
Pontaaaa <jonathan.stenstrom@hotmail.com>
2023-11-02 00:09:10 +0100

    add docstring to hangman state, add more comments

--- run.py
+++ run.py
@@ -5,14 +5,17 @@ hangman game
 import random
 import re
 
-# Lists of words for the game
+# lists of words for the game
 easy_words = ["lion", "tiger", "horse", "bear", "rabbit",
              "koala", "panda", "snake", "otter", "raccoon"]
 hard_words = ["kangaroo", "penguin", "cheetah", "dolphin", "chameleon",
              "platypus", "armadillo", "capybara", "fennec", "axolotl"]
 
-# Function to display the hangman figure
+# function to display the hangman
 def display_hangman(hangman_state):
+    """
+    display the hangman based on the hangman_state.
+    """
     hangman_parts = [
         "  +---+\n      |\n      |\n      |\n      |\n      |\n=========",
         "  +---+\n  |   |\n      |\n      |\n      |\n      |\n=========",
@@ -26,7 +29,7 @@ def display_hangman(hangman_state):
 
     print(hangman_parts[hangman_state])
 
-# Function to play the Hangman game
+# function to play the game
 def play_hangman():
     """
     play Hangman with user interaction.
@@ -45,35 +48,35 @@ def play_hangman():
         else:
             print("Invalid input. Please enter 'easy' or 'hard'.")
 
-    # Choose a random word from the selected word list
+    # randomly selected word from word list
     word = random.choice(words)
 
-    # Initialize the variables
+    # initialize the variables
     guesses = ""
     max_attempts = 7
 
-    # Initialize the hangman state
+    # initialize the hangman state
     hangman_state = 0
 
-    # Display the initial hangman figure
+    # display the initial hangman
     display_hangman(hangman_state)
 
-    # Main game loop
+    # main game loop
     while max_attempts > 0:
-        # Initialize the display word
+        # initialize the display word
         display_word = ""
 
-        # For each letter in the word
+        # for the letters in the word
         for letter in word:
             if letter in guesses:
                 display_word += letter  # The letter has been guessed
             else:
                 display_word += "_ "  # The letter is not guessed yet
 
-        # Print the current state of the word
+        # print the current state of the word
         print(display_word)
 
-        # Ask the player to guess a letter
+        # guess the letter
         while True:
             guess = input("Guess a letter: ").lower()
             if re.match("^[a-z]$", guess) and guess not in guesses:
@@ -83,45 +86,45 @@ def play_hangman():
             else:
                 print("Invalid input. Please enter a single letter.")
 
-        # Add the guess to the list of guesses
+        # add the guess to the list of guesses
         guesses += guess
 
-        # Check if the guess is correct
+        # check if the guess is correct
         if guess not in word:
             max_attempts -= 1
             print(f"Wrong guess! {max_attempts} attempts left.")
             hangman_state += 1
 
-        # Display the hangman figure
+        # display the hangman
         display_hangman(hangman_state)
 
-        # Check if the player has won
+        # check if player has won
         if set(word).issubset(set(guesses)):
             print(f"Congratulations {player_name}! You guessed the word: {word}")
             return True  # Return True for a win
 
-    # If the player has used all attempts
+    # if player has used all their attempts
     if max_attempts == 0:
         print(f"Out of attempts! The word was: {word}")
         print(f"Sorry. {player_name}.")
         return False  # Return False for a loss
 
-# Welcome the player and ask for their name
+# ask player for their name and welcome them
 player_name = input("What's your name? ")
 print(f"Hi {player_name}, welcome to Hangman!")
 
-# Initialize win and loss counters
+# initialize win and loss counters
 WINS = 0
 LOSSES = 0
 
-# Main game loop with restart option
+# restart option
 while True:
     if play_hangman():
         WINS += 1
     else:
         LOSSES += 1
 
-    # Display win and loss counters
+    # display win and loss counters
     print(f"Total Wins: {WINS}, Total Losses: {LOSSES}")
 
     play_again = input("Do you want to play again? (yes/no): ").lower()

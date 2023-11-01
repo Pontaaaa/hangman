835d5dee9644d6f029d10417a41bb98de66ece21
Pontaaaa <jonathan.stenstrom@hotmail.com>
2023-11-01 22:58:24 +0100

    add import random for choice and words for hangman

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+# lists of words
+easy_words = ["lion", "tiger", "horse", "bear", "rabbit", "koala", "panda", "snake", "otter", "raccoon"]
+hard_words = ["kangaroo", "penguin", "cheetah", "dolphin", "chameleon", "platypus", "armadillo", "capybara", "fennec", "axolotl"]
